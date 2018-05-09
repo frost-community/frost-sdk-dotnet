83567c16b4afd2c19f1140af5935cdb5a1c65062
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-05-09 22:16:17 +0900

    Refactoring IApiRequester.cs

--- FrostSDK/IApiRequester.cs
+++ FrostSDK/IApiRequester.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,6 +6,6 @@ namespace FrostSDK
 {
 	public interface IApiRequester
 	{
-		Task<ApiResponse> RequestAsync(HttpMethod method, string endpoint, string accessToken, Dictionary<string, string> parameters = null, Dictionary<string, string> headers = null);
+		Task<ApiResponse> RequestAsync(HttpMethod method, string endpoint, string accessToken, IDictionary<string, string> parameters = null, IDictionary<string, string> headers = null);
 	}
 }
