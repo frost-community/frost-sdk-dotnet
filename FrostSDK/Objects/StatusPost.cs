a8dd9f7f2b838e6b637ee3c4cb7157522503b774
marihachi <marihachi0620@gmail.com>
2018-05-11 02:42:57 +0900

    update objects

--- FrostSDK/Objects/StatusPost.cs
+++ FrostSDK/Objects/StatusPost.cs
@@ -1,18 +1,32 @@
 ﻿using Newtonsoft.Json;
+using System;
 
 namespace FrostSDK.Objects
 {
 	public class StatusPost
 	{
+		private StatusPost() { }
+
+		/// <summary>
+		/// JSONデータからクラスのインスタンスを生成します。
+		/// </summary>
 		public static StatusPost FromJson(string json)
 		{
 			return JsonConvert.DeserializeObject<StatusPost>(json);
 		}
 
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
 		[JsonProperty("createdAt")]
-		public long CreatedAt { get; set; }
+		public long CreatedAtRaw { get; set; }
+
+		public DateTime CreatedAt { get => Utility.ParseUnixTime(CreatedAtRaw); }
 
 		[JsonProperty("text")]
 		public string Text { get; set; }
+
+		[JsonProperty("user")]
+		public User User { get; set; }
 	}
 }
--- FrostSDK/Objects/User.cs
+++ FrostSDK/Objects/User.cs
@@ -1,6 +1,35 @@
-﻿namespace FrostSDK.Objects
+﻿using Newtonsoft.Json;
+using System;
+
+namespace FrostSDK.Objects
 {
     public class User
     {
-    }
+		private User() { }
+
+		/// <summary>
+		/// JSONデータからクラスのインスタンスを生成します。
+		/// </summary>
+		public static User FromJson(string json)
+		{
+			return JsonConvert.DeserializeObject<User>(json);
+		}
+
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("createdAt")]
+		public long CreatedAtRaw { get; set; }
+
+		public DateTime CreatedAt { get => Utility.ParseUnixTime(CreatedAtRaw); }
+
+		[JsonProperty("screenName")]
+		public string ScreenName { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("description")]
+		public string Description { get; set; }
+	}
 }
